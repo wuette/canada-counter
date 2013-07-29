18f07e5c31638d7f0cec11089b072e992301d5db
Thomas Poblete <thomas.pob@gmail.com>
2013-07-28 23:50:11 -0400

    First alpha

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-    
+
+    var max = counter;
+
+    console.log(max);
+
+    function assignNumber(number) {
+        $(".number:last").text(number);
+    }
+
+    $(".arrow_next").click(function() {
+        
+        if(counter < max) counter++;
+        else counter = 1;
+
+        assignNumber(counter);
+        
+        $("html").css("background-image", "url('../content/" + counter + ".jpg'");
+    });
+
+    $(".arrow_previous").click(function() {
+
+        if(counter > 1) counter--;
+        else counter = max;
+
+        assignNumber(counter);
+
+        $("html").css("background-image", "url('../content/" + counter + ".jpg'");
+    });
 })
